8f27fcfd27d36c1718b349c60671440dc7825a94
irinabudiakina <ibudiakina@gmail.com>
2026-03-04 20:12:54 +0300

    added post request

--- src/setup-app.ts
+++ src/setup-app.ts
@@ -1,5 +1,6 @@
 import express, { Express } from "express";
 import {db} from "./db/in-memories.db";
+import {Video, VideoResolution} from "./drivers/types/video";
 
 export const setupApp = (app: Express) => {
     app.use(express.json());
@@ -20,6 +21,22 @@ export const setupApp = (app: Express) => {
 
         res.status(200).send(video);
     });
+
+    app.post("/videos", (req, res) => {
+        const newVideo: Video = {
+            id: db.videos.length ? db.videos[db.videos.length - 1].id + 1 : 1,
+            title: req.body.title,
+            author: req.body.author,
+            canBeDownloaded: false,
+            minAgeRestriction: null,
+            createdAt: new Date().toString(),
+            publicationDate:  new Date().toString(),
+            availableResolutions: req.body.availableResolutions,
+        };
+
+        db.videos.push(newVideo);
+        res.status(201).send(newVideo);
+    })
 
     return app;
 };
